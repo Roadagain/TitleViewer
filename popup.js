ae7dd182b30f98fbb0817a497aa6f016213d1c72
Roadagain <ringoh72@gmail.com>
2016-05-11 13:02:09 +0900

    Copy title to clipboard

--- popup.js
+++ popup.js
@@ -1,6 +1,15 @@
 window.addEventListener('load', function(){
   var elem = document.getElementById('page_title');
+  var button = document.getElementById('copy');
   chrome.tabs.query({ active: true }, function(result){
     elem.innerHTML = result[0].title;
+    button.addEventListener('click', function(){
+      var textArea = document.createElement('textarea');
+      document.body.appendChild(textArea);
+      textArea.value = result[0].title;
+      textArea.select();
+      document.execCommand('copy');
+      document.body.removeChild(textArea);
+    });
   });
 });
