7dd86b2b3b59e088bb3895ef3f79f28b24d7ec14
Roadagain <ringoh72@gmail.com>
2016-05-11 13:25:03 +0900

    Comment

--- popup.js
+++ popup.js
@@ -1,15 +1,22 @@
 window.addEventListener('load', function(){
   'use strict';
 
+  // Get elements
   var elem = document.getElementById('page_title');
   var button = document.getElementById('copy');
+
+  // Get tab info
   chrome.tabs.query({ active: true }, function(result){
     'use strict';
 
+    // Show title
     elem.innerHTML = result[0].title;
+
+    // Add click listener to copy button
     button.addEventListener('click', function(){
       'use strict';
 
+      // Copy title to clipboard
       var textArea = document.createElement('textarea');
       document.body.appendChild(textArea);
       textArea.value = result[0].title;
