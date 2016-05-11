a912161c1a3f07325c96c05da9501acb71859842
Roadagain <ringoh72@gmail.com>
2016-05-11 13:23:10 +0900

    Use strict

--- popup.js
+++ popup.js
@@ -1,9 +1,15 @@
 window.addEventListener('load', function(){
+  'use strict';
+
   var elem = document.getElementById('page_title');
   var button = document.getElementById('copy');
   chrome.tabs.query({ active: true }, function(result){
+    'use strict';
+
     elem.innerHTML = result[0].title;
     button.addEventListener('click', function(){
+      'use strict';
+
       var textArea = document.createElement('textarea');
       document.body.appendChild(textArea);
       textArea.value = result[0].title;
