ba66225dc1fc2d7b24d52b870da68950bd6ccf0f
Roadagain <ringoh72@gmail.com>
2016-05-11 13:26:38 +0900

    Rename variables

--- popup.js
+++ popup.js
@@ -2,24 +2,24 @@ window.addEventListener('load', function(){
   'use strict';
 
   // Get elements
-  var elem = document.getElementById('page_title');
-  var button = document.getElementById('copy');
+  var title = document.getElementById('page_title');
+  var copy = document.getElementById('copy');
 
   // Get tab info
-  chrome.tabs.query({ active: true }, function(result){
+  chrome.tabs.query({ active: true }, function(tabs){
     'use strict';
 
     // Show title
-    elem.innerHTML = result[0].title;
+    title.innerHTML = tabs[0].title;
 
     // Add click listener to copy button
-    button.addEventListener('click', function(){
+    copy.addEventListener('click', function(){
       'use strict';
 
       // Copy title to clipboard
       var textArea = document.createElement('textarea');
       document.body.appendChild(textArea);
-      textArea.value = result[0].title;
+      textArea.value = tabs[0].title;
       textArea.select();
       document.execCommand('copy');
       document.body.removeChild(textArea);
