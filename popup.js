5ac4752607c9509a6a9cacc70c5bb6da72e629a6
Roadagain <ringoh72@gmail.com>
2016-05-12 11:43:34 +0900

    Copy url to clipboard

--- popup.js
+++ popup.js
@@ -5,6 +5,7 @@ window.addEventListener('load', function(){
   var title = document.getElementById('page_title');
   var url = document.getElementById('page_url');
   var copyTitle = document.getElementById('copy_title');
+  var copyUrl = document.getElementById('copy_url');
 
   // Get tab info
   chrome.tabs.query({ active: true }, function(tabs){
@@ -22,6 +23,17 @@ window.addEventListener('load', function(){
       var textArea = document.createElement('textarea');
       document.body.appendChild(textArea);
       textArea.value = tabs[0].title;
+      textArea.select();
+      document.execCommand('copy');
+      document.body.removeChild(textArea);
+    });
+    copyUrl.addEventListener('click', function(){
+      'use strict';
+
+      // Copy title to clipboard
+      var textArea = document.createElement('textarea');
+      document.body.appendChild(textArea);
+      textArea.value = tabs[0].url;
       textArea.select();
       document.execCommand('copy');
       document.body.removeChild(textArea);
